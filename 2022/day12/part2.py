d34d52623b7bfd61dd89a2a74dcbc598ebb9d702
Alex Valenti <alex.valenti@studenti.unimi.it>
2022-12-13 03:44:39 +0100

    Aggiunti i file del dodicesimo giorno

---
+++ 2022/day12/part2.py
@@ -0,0 +1,84 @@
+class Graph:
+    __adjacent = dict()
+
+    def addEdge(self, a, b):
+        if a not in self.__adjacent:
+            self.__adjacent[a] = [a]
+        self.__adjacent[a].append(b)
+        if a != b:
+            if b not in self.__adjacent:
+                self.__adjacent[b] = []
+
+    def bfs(self, start, goal):
+        visited = set()
+        tovisit = [start]
+        came_from = {start: 0}
+
+        while len(tovisit) > 0:
+            x = tovisit.pop(0)
+
+            if x == goal:
+                return came_from[x]
+
+            visited.add(x)
+            for y in self.__adjacent[x]:
+                if y in visited:
+                    continue
+
+                if y not in tovisit:
+                    tovisit.append(y)
+
+                came_from[y] = came_from[x]+1
+
+        return 0
+
+
+def reacble(mapp: list[list[int]], i: int, j: int) -> list[tuple[int, int]]:
+    v = mapp[i][j]
+    res = list[tuple[int, int]]()
+    if i+1 < len(mapp) and mapp[i+1][j] - v <= 1:
+        res.append((i+1, j))
+    if i-1 >= 0 and mapp[i-1][j] - v <= 1:
+        res.append((i-1, j))
+    if j+1 < len(mapp[i]) and mapp[i][j+1] - v <= 1:
+        res.append((i, j+1))
+    if j-1 >= 0 and mapp[i][j-1] - v <= 1:
+        res.append((i, j-1))
+
+    return res
+
+
+def part2(file: list[str]):
+    mapp = []
+    starts = []
+    for i in range(len(file)):
+        mapp.append([])
+        for j in range(len(file[i])):
+            mapp[i].append(ord(file[i][j])-ord("a"))
+
+            if file[i][j] == "a":
+                starts.append((i, j))
+
+            if file[i][j] == "S":
+                start = (i, j)
+                mapp[i][j] = 0
+
+            elif file[i][j] == "E":
+                goal = (i, j)
+                mapp[i][j] = ord("z")-ord("a")
+
+
+    g = Graph()
+
+    for i in range(len(mapp)):
+        for j in range(len(mapp[i])):
+            for p in reacble(mapp, i, j):
+                g.addEdge((i, j), p)
+
+    min = g.bfs(start, goal)
+    for start in starts:
+        other = g.bfs(start, goal)
+        if other != 0 and other < min:
+            min = other
+
+    print(min)
