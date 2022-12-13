443821f68b7342be863e1d2d283374552996cf2c
Alex Valenti <valentivaledx@gmail.com>
2022-12-13 19:07:02 +0100

    Aggiunta la enumerate e sfruttato meglio il linguaggio python

--- 2022/day05/part2.py
+++ 2022/day05/part2.py
@@ -2,26 +2,21 @@ def move(stacks: list[list[str]], f, t, n=1):
     stacks[t].extend(stacks[f][-n:])
     stacks[f] = stacks[f][:-n]
 
-def readStack(file, stacks):
-    for line in file:
-        if "[" not in line:
-            break
-        for i in range(len(stacks)):
-            if line[i*4+1] != " ":
-                stacks[i].insert(0, line[i*4+1])
-            i += 1
-    return i+1
 
 def part2(file: list[str]):
     stacks = [[] for x in range((len(file[0]) + 1) // 4)]
 
-    i = readStack(file, stacks)
+    for i, line in enumerate(file):
+        if "[" not in line:
+            break
+        for j, stack in enumerate(stacks):
+            if line[j*4+1] != " ":
+                stack.insert(0, line[j*4+1])
 
-    for line in file[i:]:
+    for line in file[i+2:]:
         _, n, _, f, _, t = line.split()
         move(stacks, int(f)-1, int(t)-1, int(n))
 
     for v in stacks:
         print(v[-1], end="")
     print()
-	
