3dcae121fa7c45d3a5a96b95b2a0da17fb61e938
Alex Valenti <valentivaledx@gmail.com>
2022-12-14 14:58:49 +0100

    Sempre brutta ma più organizzata

--- 2022/day14/part1.py
+++ 2022/day14/part1.py
@@ -22,45 +22,43 @@ def dropp(mapp, s) -> bool:
         return dropp(mapp, s)
     s[1]-=1
 
+
+def expandMap(mapp, t, l, r, minx, maxx):
+    if t >= len(mapp):
+        for _ in range(len(mapp), t+1):
+            mapp.append([])
+
+    if r >= maxx:
+        maxx = r
+    
+    for i in range(len(mapp)):
+        if maxx-minx >= len(mapp[i]) :
+            for _ in range(len(mapp[i]), maxx+1-minx):
+                mapp[i].append(".")
+        if l < minx:
+            for _ in range(l, minx):
+                mapp[i].insert(0, ".")
+    
+    if l < minx:
+        minx = l
+    
+    return minx, maxx
+
+def draw(mapp, p1, p2, minx):
+    for i in range(p1[1], p2[1]+1):
+        for j in range(p1[0]-minx, p2[0]+1-minx):
+            mapp[i][j] = "#"
+
 def part1(file: list[str]):
     mapp = list()
-    segments = list()
     minx, maxx = 500, 0
     for line in file:
         points = [[int(x) for x in ps.split(",")] for ps in line.split(" -> ")]
 
         for i in range(1, len(points)):
-            if points[i-1][1] < points[i][1]:
-                d, t = points[i-1][1], points[i][1]
-            else:
-                d, t = points[i][1], points[i-1][1]
-
-            if t >= len(mapp):
-                for _ in range(len(mapp), t+1):
-                    mapp.append([])
-
-            if points[i-1][0] < points[i][0]:
-                l, r = points[i-1][0], points[i][0] 
-            else:
-                l, r = points[i][0], points[i-1][0]
-            
-
-            if r >= maxx:
-                maxx = r
-            
-            for i in range(len(mapp)):
-                if l < minx:
-                    for _ in range(l, minx):
-                        mapp[i].insert(0, ".")
-                if maxx-minx >= len(mapp[i]) :
-                    for _ in range(len(mapp[i]), maxx+1-minx):
-                        mapp[i].append(".")
-            if l < minx:
-                minx = l
-            
-            for i in range(d, t+1):
-                for j in range(l-minx, r+1-minx):
-                        mapp[i][j] = "#"
+            p1, p2 = sorted(points[i-1:i+1])
+            minx, maxx = expandMap(mapp, p2[1], p1[0], p2[0], minx, maxx)
+            draw(mapp, p1, p2, minx)
     
     i, s = 0, [500-minx, 0]
     while not dropp(mapp, s):
