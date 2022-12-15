c7e75cda5bc4253cd964c943e2c8743f3d0173f4
Alex Valenti <valentivaledx@gmail.com>
2022-12-15 19:13:57 +0100

    Cambiata filosofia: tempi migliorati

--- 2022/day15/part1.py
+++ 2022/day15/part1.py
@@ -1,37 +1,45 @@
 import re
 
-class Sensor:
-    def __init__(self, sensor, beacon) -> None:
-        self.__sensor = sensor
-        self.beacon = beacon
-        self.radius = self.distance(beacon)
+def distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
-    def distance(self, p1):
-        return abs(self.__sensor[0] - p1[0]) + abs(self.__sensor[1] - p1[1])
+def inside(points, sensor, beacon, y):
+    remain = distance(sensor, beacon) - abs(sensor[1] - y)
+    if remain < 0:
+        return
 
-    def isCloser(self, p1) -> bool:
-        return self.distance(p1) <= self.radius
+    points[beacon] = "B" 
+    points[sensor] = "S" 
+    
+    for x in range(sensor[0]-remain, sensor[0]+remain+1):
+        if (x, y) not in points:
+            points[(x, y)] = "#" 
+    
+    
 
-def part1(file: list[str]):
-    sensors = list[Sensor]()
-    minx, maxx = 2**16, 0
-
-    for line in file:
-        line = [int(x) for x in re.split('Sensor at x=|, y=|: closest beacon is at x=|, y=', line)[1:]]
-        sensor = Sensor((line[0], line[1]), (line[2], line[3]))
-        sensors.append(sensor)
-        maxx = max(maxx, line[0]+sensor.radius, line[2])
-        minx = min(minx, line[0]-sensor.radius, line[2])
-
-    count, y = 0, 10#2000000
+def printLine(points, y):
+    minx = min(p[0] for p in points)
+    maxx = max(p[0] for p in points)
 
     for x in range(minx, maxx+1):
-        if (x, y) == sensor or (x, y) == sensor.beacon:
-            continue
-        for sensor in sensors:
-            if sensor.isCloser((x, y)):
-                count+=1
-                break
-    
-    print("l:",maxx+1-minx)  
+        if (x,y) in points:
+            print(points[(x, y)], end="")
+        else:
+            print(end=".")
+    print()
+
+
+def part1(file: list[str]):
+    points = dict()
+
+    y = 2000000 
+    for line in file:
+        line = [int(x) for x in re.split('Sensor at x=|, y=|: closest beacon is at x=|, y=', line)[1:]]
+        
+        inside(points, (line[0], line[1]), (line[2], line[3]), y)
+
+    count = 0
+    for point in points:
+        if points[point] == "#":
+            count+=1
     print(count)
