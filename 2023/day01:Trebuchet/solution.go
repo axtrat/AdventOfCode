ad4537725940ce67a581a98e148a5e00dcb761fc
axtrat <valentivaledx+github@gmail.com>
2023-12-01 17:48:34 +0100

    Soluzione seconda parte

--- 2023/day01:Trebuchet/solution.go
+++ 2023/day01:Trebuchet/solution.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode"
 )
+
+var numbers []string = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
 func Part1(file []string) {
 	var sum int
@@ -26,6 +29,41 @@ func Part1(file []string) {
 	fmt.Println(sum)
 }
 
+func searchFirst(str string) int {
+	for i := 0; i < len(str); i++ {
+		if unicode.IsDigit(rune(str[i])) {
+			return int(str[i] - '0')
+		}
+
+		for j, number := range numbers {
+			if strings.HasPrefix(string(str[i:]), number) {
+				return j + 1
+			}
+		}
+	}
+	return -1
+}
+
+func searchLast(str string) int {
+	for i := len(str) - 1; i >= 0; i-- {
+		if unicode.IsDigit(rune(str[i])) {
+			return int(str[i] - '0')
+		}
+
+		for j, number := range numbers {
+			if strings.HasSuffix(string(str[:i+1]), number) {
+				return j + 1
+			}
+		}
+	}
+	return -1
+}
+
 func Part2(file []string) {
-	fmt.Println("Non Implementata")
+	var sum int
+	for _, riga := range file {
+		var first, last int = searchFirst(riga), searchLast(riga)
+		sum += first*10 + last
+	}
+	fmt.Println(sum)
 }
