7c8f8e014291b68808176a8fcfc6d3fa3615e9a9
axtrat <valentivaledx+github@gmail.com>
2023-12-01 19:41:21 +0100

    Refactoring del codice

--- 2023/day01:Trebuchet/solution.go
+++ 2023/day01:Trebuchet/solution.go
@@ -8,61 +8,51 @@ import (
 
 var numbers []string = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 
+func searchFirst(str string, words bool) int {
+	for i := 0; i < len(str); i++ {
+		if unicode.IsDigit(rune(str[i])) {
+			return int(str[i] - '0')
+		}
+		if words {
+			for j, number := range numbers {
+				if strings.HasPrefix(string(str[i:]), number) {
+					return j + 1
+				}
+			}
+		}
+	}
+	return -1
+}
+
+func searchLast(str string, words bool) int {
+	for i := len(str) - 1; i >= 0; i-- {
+		if unicode.IsDigit(rune(str[i])) {
+			return int(str[i] - '0')
+		}
+		if words {
+			for j, number := range numbers {
+				if strings.HasSuffix(string(str[:i+1]), number) {
+					return j + 1
+				}
+			}
+		}
+	}
+	return -1
+}
+
 func Part1(file []string) {
 	var sum int
 	for _, riga := range file {
-		var first, last int
-		for i := 0; i < len(riga); i++ {
-			if unicode.IsDigit(rune(riga[i])) {
-				first = int(riga[i] - '0')
-				break
-			}
-		}
-		for i := len(riga) - 1; i >= 0; i-- {
-			if unicode.IsDigit(rune(riga[i])) {
-				last = int(riga[i] - '0')
-				break
-			}
-		}
+		var first, last int = searchFirst(riga, false), searchLast(riga, false)
 		sum += first*10 + last
 	}
 	fmt.Println(sum)
 }
 
-func searchFirst(str string) int {
-	for i := 0; i < len(str); i++ {
-		if unicode.IsDigit(rune(str[i])) {
-			return int(str[i] - '0')
-		}
-
-		for j, number := range numbers {
-			if strings.HasPrefix(string(str[i:]), number) {
-				return j + 1
-			}
-		}
-	}
-	return -1
-}
-
-func searchLast(str string) int {
-	for i := len(str) - 1; i >= 0; i-- {
-		if unicode.IsDigit(rune(str[i])) {
-			return int(str[i] - '0')
-		}
-
-		for j, number := range numbers {
-			if strings.HasSuffix(string(str[:i+1]), number) {
-				return j + 1
-			}
-		}
-	}
-	return -1
-}
-
 func Part2(file []string) {
 	var sum int
 	for _, riga := range file {
-		var first, last int = searchFirst(riga), searchLast(riga)
+		var first, last int = searchFirst(riga, true), searchLast(riga, true)
 		sum += first*10 + last
 	}
 	fmt.Println(sum)
