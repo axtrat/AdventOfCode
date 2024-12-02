989dfe185ca56a56b6597a42319fde8a2d3294eb
axtrat <valentivaledx+github@gmail.com>
2024-12-02 17:08:46 +0100

    Soluzione a parte 2 inefficiente

--- 2024/day02_Red-Nosed_Reports/main.py
+++ 2024/day02_Red-Nosed_Reports/main.py
@@ -2,24 +2,32 @@ import sys
 
 
 def isSafe(report: list[int]) -> bool:
-    sign = -1 if report[0] < report[-1] else 1
+    sign = -1 if report[0] < report[1] else 1
     for i in range(len(report)-1):
         diff = (report[i] - report[i+1]) * sign
         if diff < 1 or diff > 3:
             return False
     return True
 
+def isSafe2(report: list[int]) -> bool:
+    sign = -1 if report[0] < report[1] else 1
+    for i in range(len(report)-1):
+        diff = (report[i] - report[i+1]) * sign
+        if diff < 1 or diff > 3:
+            return isSafe(report[1:]) or isSafe(report[:i] + report[i+1:]) or isSafe(report[:i+1] + report[i+2:])
+    return True
 
 def part1(file: list[str]):
-    #reports = [[int(num) for num in line.split()] for line in file]
-    reports = list(map(lambda line: list(map(int, line.split())), file))
+    reports = map(lambda line: list(map(int, line.split())), file)
 
     res = len(list(filter(isSafe, reports)))
     print(res)
 
 def part2(file: list[str]):
-    pass
-    #print(res)
+    reports = map(lambda line: list(map(int, line.split())), file)
+
+    res = len(list(filter(isSafe2, reports)))
+    print(res)
 
 def main():
     filename = sys.argv[1]+".txt"
