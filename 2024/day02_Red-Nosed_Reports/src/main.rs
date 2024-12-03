d17bd718c396325779486bc24474fd4d3d4c9d9a
axtrat <valentivaledx+github@gmail.com>
2024-12-03 18:32:23 +0100

    Soluzione part2 al day02 rifatta in rust

--- 2024/day02_Red-Nosed_Reports/src/main.rs
+++ 2024/day02_Red-Nosed_Reports/src/main.rs
@@ -1,11 +1,14 @@
 use std::io::stdin;
 
-fn is_safe(report: &Vec<i32>) -> bool {
+fn is_safe(report: &Vec<i32>, tollerate: bool) -> bool {
     let sign = if report[0] < report[1] { -1 } else { 1 };
     for i in 0..report.len()-1 {
         let diff = (report[i] - report[i+1]) * sign;
         if diff < 1 || diff > 3 {
-            return false;
+            if !tollerate {
+                return false;
+            }
+            return is_safe(&report[1..].to_vec(), false) || is_safe(&[&report[..i], &report[i+1..]].concat(), false) || is_safe(&[&report[..i+1], &report[i+2..]].concat(), false);
         }
     }
     return true;
@@ -14,7 +17,15 @@ fn is_safe(report: &Vec<i32>) -> bool {
 fn part1(reports: &Vec<Vec<i32>>) {
     let res = reports
         .iter()
-        .filter(|report| is_safe(report))
+        .filter(|report| is_safe(report, false))
+        .count();
+    println!("{}", res);
+}
+
+fn part2(reports: &Vec<Vec<i32>>) {
+    let res = reports
+        .iter()
+        .filter(|report| is_safe(report, true))
         .count();
     println!("{}", res);
 }
@@ -37,5 +48,5 @@ fn main() {
         
 
     part1(&reports);
-    //part2(&file);
+    part2(&reports);
 }
