a357be1ac84e0305b0bcf3b31759aea1b6123f68
axtrat <valentivaledx+github@gmail.com>
2024-12-03 10:28:36 +0100

    Filtro i gruppi ispirandomi alla soluzione di Mattia Oldani

--- 2024/day03_Mull_It_Over/main.py
+++ 2024/day03_Mull_It_Over/main.py
@@ -1,33 +1,29 @@
 import sys
 import re
 
-def part1(file: list[str]):
-    tuples = re.findall("mul\((\d+),(\d+)\)","".join(file))
+def part1(file: str):
+    tuples = re.findall(r"mul\((\d+),(\d+)\)",file)
 
     res = sum(map(lambda tuple: int(tuple[0])*int(tuple[1]), tuples))
     
     print(res)
 
-def part2(file: list[str]):
-    pattern = re.compile("do\(\)|don't\(\)|mul\((\d+),(\d+)\)")
+def part2(file: str):
+    pattern = re.compile(r"(do\(\)|don't\(\))|mul\((\d+),(\d+)\)")
 
     enable = True
     res = 0
-    for mt in re.finditer(pattern, "".join(file)):
-        match mt.group(), mt.groups():
-            case ("do()", _):
-                enable = True
-            case ("don't()", _):
-                enable = False
-            case (_, (x, y)):
-                if enable:
-                    res += int(x) * int(y)
-    
+    for mt in re.finditer(pattern, file):
+        match list(filter(lambda x: x != None, mt.groups())), enable:
+            case ["do()"], _: enable = True
+            case ["don't()"], _: enable = False
+            case [a,b], True: res += int(a)*int(b)
+
     print(res)
 
 
 with open(sys.argv[1]+".txt", "r") as file:
-    lines = [line.rstrip("\n") for line in file]
+    lines = file.read()
 
 part1(lines)
 part2(lines)
