7d322ae33cb0c0fa865336f46b820205d0c84833
axtrat <valentivaledx+github@gmail.com>
2024-12-03 08:47:32 +0100

    modifiche estetiche al main

--- 2024/day01_Historian_Hysteria/main.py
+++ 2024/day01_Historian_Hysteria/main.py
@@ -1,10 +1,9 @@
-import re
 import sys
 from collections import Counter
 
 
 def part1(file: list[str]):
-    l1, l2 = zip(*(re.split("\s+", line) for line in file))
+    l1, l2 = zip(*(line.split() for line in file))
     l1, l2 = sorted(l1), sorted(l2)
 
     res = sum([abs(int(l1[i]) - int(l2[i])) for i in range(len(l1))])
@@ -12,24 +11,16 @@ def part1(file: list[str]):
     print(res)
 
 def part2(file: list[str]):
-    l1, l2 = zip(*(re.split("\s+", line) for line in file))
+    l1, l2 = zip(*(line.split() for line in file))
     dict2 = Counter(l2)
 
     res = sum(int(num) * dict2[num] for num in l1 if num in dict2)
 
     print(res)
-    
-
-def main():
-    filename = sys.argv[1]+".txt"
-
-    with open(filename, "r") as file:
-        lines = [line.rstrip("\n") for line in file]
-    
-    
-    part1(lines)
-    part2(lines)
 
 
-if __name__ == "__main__":
-    main()
+with open(sys.argv[1]+".txt", "r") as file:
+    lines = [line.rstrip("\n") for line in file]
+
+part1(lines)
+part2(lines)
