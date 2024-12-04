0459176615eae7397cb299f19c043cc479408f39
axtrat <valentivaledx+github@gmail.com>
2024-12-04 15:27:36 +0100

    Pulizia parte1

--- 2024/day04_Ceres_Search/src/main.rs
+++ 2024/day04_Ceres_Search/src/main.rs
@@ -1,31 +1,44 @@
 use std::io::stdin;
 
-fn update_buffer(buffer: &mut String, char: char, n: usize) {
-    buffer.push(char);
-    if buffer.len() > n {
-        buffer.remove(0);
-    }
-}
-
 fn find(file: &Vec<Vec<char>>) -> i32{
     let (n, m) = (file.len(), file[0].len());
+    let mut sum = 0;
+    for dr in vec![(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] {
+        for i in 0..m {
+            for j in 0..n {
+                sum += (0..4)
+                    .filter_map(|k| {
+                        let ni = i as isize + k * dr.0;
+                        let nj = j as isize + k * dr.1;
+                        file.get(ni as usize)
+                            .and_then(|line| {
+                            line.get(nj as usize)
+                        })
+                    })
+                    .collect::<String>()
+                    .matches("XMAS")
+                    .count() as i32;
+            }
+        }
+    }
+    sum
+}
 
-    let mut sum = file
-        .iter()
-        .map(|line| {
-            line.iter()
+fn find2(file: &Vec<Vec<char>>) -> i32 {
+    let (n, m) = (file.len(), file[0].len());
+    let mut sum = 0;
+    for i in 0..m-2 {
+        for j in 0..n-2 {
+            sum += (0..3)
+                .filter_map(|k| {
+                    if file[i+k][j+k] == file[i+k][j+2-k] {
+                        Some(file[i+k][j+k])
+                    } else {
+                        None
+                    }
+                })
                 .collect::<String>()
-                .matches("XMAS")
-                .count() as i32
-        })
-        .sum::<i32>();
-
-    for i in 0..m-3 {
-        for j in 0..n-3 {
-            sum += (0..4)
-                .map(|k| file[i+k][j+k])
-                .collect::<String>()
-                .matches("XMAS")
+                .matches("MAS")
                 .count() as i32;
         }
     }
@@ -43,37 +56,9 @@ fn rotate_matrix(matrix: &Vec<Vec<char>>) -> Vec<Vec<char>> {
     new_matrix
 }
 
-fn find2(file: &Vec<Vec<char>>) -> i32 {
-    let (n, m) = (file.len(), file[0].len());
-    let mut sum = 0;
-    for i in 0..m-2 {
-        for j in 0..n-2 {
-            sum += (0..3)
-                .map(|k| {
-                    if file[i+k][j+k] == file[i+k][j+2-k] {
-                        file[i+k][j+k]
-                    } else {
-                        ' '
-                    }
-                })
-                .collect::<String>()
-                .matches("MAS")
-                .count() as i32;
-        }
-    }
-
-    sum
-}
 
 fn part1(file: &Vec<Vec<char>>) {
-    let mut sum = 0;
-    let mut matrix = file.clone();
-    for _ in 0..4 {
-        sum += find(&matrix);
-        matrix = rotate_matrix(&matrix);
-    }
-
-    println!("{}", sum);
+    println!("{}", find(file));
 }
 
 fn part2(file: &Vec<Vec<char>>) {
